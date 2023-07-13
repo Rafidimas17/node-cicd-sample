33ecf6b5df806e0bdeb8d4465d64f063255a74df
Rafidimas17 <rafidimas1706@gmail.com>
2023-07-13 14:45:25 +0700

    new changes

---
+++ server.test.js
@@ -0,0 +1,37 @@
+const app = require('./server') // Link to your server file
+const supertest = require('supertest')
+const request = supertest(app)
+
+
+it('Call the endpoint /home', (done) => {
+    request
+      .get('/youtube')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).toBe('Hello, youtube indonesia!')
+        if (err) return done(err);
+        done();
+      });
+  });
+
+it('Call the endpoint /ping', (done) => {
+    request
+      .get('/ping')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).toBe('Pong!')
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('Call the endpoint /', (done) => {
+    request
+      .get('/')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).toBe('This App is running properly!')
+        if (err) return done(err);
+        done();
+      });
+  });
